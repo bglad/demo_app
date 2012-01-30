3048a6316aeed84694cf2873e35833b473da6bd7
Bret Glad <gladfelty@gmail.com>
2012-01-30 12:25:56 -0800

    Finish demo app

--- db/schema.rb
+++ db/schema.rb
@@ -11,9 +11,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120127223645) do
+ActiveRecord::Schema.define(:version => 20120130190533) do
 
   create_table "microposts", :force => true do |t|
+    t.string   "content"
+    t.integer  "user_id"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
+  create_table "mircroposts", :force => true do |t|
     t.string   "content"
     t.integer  "user_id"
     t.datetime "created_at", :null => false
